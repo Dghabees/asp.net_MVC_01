46262c909de2ad3e9d870410c3d47c3ef2d24a86
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-06-27 11:13:47 +0300

    init

---
+++ asp.net_MVC_01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp.net_MVC_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
